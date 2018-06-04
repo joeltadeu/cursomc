f1a2494f7d31aa4a737f8e16ed305f7a16d67301
Joel Silva <joeltadeu@gmail.com>
2018-06-04 01:31:14 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -11,7 +12,10 @@ public class PagamentoComBoleto extends Pagamento {
 
 	private static final long serialVersionUID = 2032089421795888400L;
 
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
