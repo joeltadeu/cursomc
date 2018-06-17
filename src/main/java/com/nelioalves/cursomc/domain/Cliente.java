e0686826a1996350b1c2cb4bd3dc84bca0018e4f
Joel Silva <joeltadeu@gmail.com>
2018-06-17 00:48:37 -0300

    Validacao customizada: email não repetido na insercao de cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,7 +18,6 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
-import com.nelioalves.cursomc.services.validation.ClienteValidation;
 
 @Entity
 public class Cliente implements Serializable {
@@ -29,6 +29,8 @@ public class Cliente implements Serializable {
 	private Integer id;
 	
 	private String nome;
+	
+	@Column(unique=true)
 	private String email;
 	private String cpfOuCnpj;
 	
--- src/main/java/com/nelioalves/cursomc/services/validation/ClienteValidator.java
+++ src/main/java/com/nelioalves/cursomc/services/validation/ClienteValidator.java
@@ -2,29 +2,44 @@ package com.nelioalves.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 import com.nelioalves.cursomc.dto.ClienteNewDTO;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
 import com.nelioalves.cursomc.resources.exceptions.FieldMessage;
 import com.nelioalves.cursomc.services.validation.util.ValidationUtil;
 
 public class ClienteValidator implements ConstraintValidator<ClienteValidation, ClienteNewDTO> {
-	
+
+	@Autowired
+	ClienteRepository repository;
+
 	@Override
 	public boolean isValid(ClienteNewDTO dto, ConstraintValidatorContext context) {
 		List<FieldMessage> errors = new ArrayList<>();
-		
-		if (dto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !ValidationUtil.isValidCPF(dto.getCpfOuCnpj())) {
+
+		if (dto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod())
+				&& !ValidationUtil.isValidCPF(dto.getCpfOuCnpj())) {
 			errors.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
-		if (dto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !ValidationUtil.isValidCNPJ(dto.getCpfOuCnpj())) {
+
+		if (dto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod())
+				&& !ValidationUtil.isValidCNPJ(dto.getCpfOuCnpj())) {
 			errors.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-		
+
+		Optional<Cliente> optionalCliente = repository.findByEmail(dto.getEmail());
+		if (optionalCliente.isPresent()) {
+			errors.add(new FieldMessage("email", "E-mail já existente"));
+		}
+
 		for (FieldMessage field : errors) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(field.getMessage()).addPropertyNode(field.getFieldName())
